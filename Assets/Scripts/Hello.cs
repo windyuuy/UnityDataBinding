f7a600299f1026a50558a208faa49df9089e58c2
windyuuy <kugoo11v12@126.com>
2022-01-17 14:28:46 +0800

    modify: 完善数据绑定demo

--- Assets/Scripts/Hello.cs
+++ Assets/Scripts/Hello.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using DataBinding;
 
@@ -7,9 +6,10 @@ public class Hello : MonoBehaviour
 {
     public void Start()
     {
-        Debug.Log("lkjwef");
+        Debug.Log("start");
         var sampleHost = new SampleHost();
 
+        // 监听表达式
         sampleHost.Watch("QQ+hello.KKK", (host, value, oldValue) =>
         {
             Debug.Log("value changed;");
@@ -17,6 +17,27 @@ public class Hello : MonoBehaviour
         });
         sampleHost.QQ = 2134;
         sampleHost.hello.KKK = 3242;
+        // 通知表达式值变化
+        vm.Tick.next();
+
+        // 监听表达式
+        sampleHost.Watch("hello.IntList[2]", (host, value, oldValue) =>
+        {
+            Debug.Log("value changed;");
+            Debug.Log(value);
+        });
+        sampleHost.hello.IntList[2] = 44;
+        // 通知表达式值变化
+        vm.Tick.next();
+
+        // 监听表达式
+        sampleHost.Watch("hello.NumDictionary[123]", (host, value, oldValue) =>
+        {
+            Debug.Log("value changed;");
+            Debug.Log(value);
+        });
+        sampleHost.hello.NumDictionary[123] = "你变了";
+        // 通知表达式值变化
         vm.Tick.next();
 
     }
