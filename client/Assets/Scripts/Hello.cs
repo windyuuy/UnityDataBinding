4949e591da14f94e7a7f15e5cd100fbe3efaec7d
windyuuy <kugoo11v12@126.com>
2022-01-23 20:23:37 +0800

    step: 更新项目设置

--- client/Assets/Scripts/Hello.cs
+++ client/Assets/Scripts/Hello.cs
@@ -35,6 +35,8 @@ public class Hello : MonoBehaviour
 		{
 			Debug.Log("value changed;");
 			Debug.Log(value);
+
+			GameObject.Find("GameObject/Canvas/Text").GetComponent<UnityEngine.UI.Text>().text = $"{value}";
 		});
 		sampleHost.hello.NumDictionary[123] = "你变了";
 		// 通知表达式值变化
