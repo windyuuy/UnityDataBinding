09de829b1e53b1a49cd95703b4b8f2aaf04098a1
windy <kugoo11v12@126.com>
2024-01-31 10:13:43 +0800

    U: commit EnhancedScroller

---
+++ client/Assets/EaseScrollView/EnhanceScrollView/Plugins/DefaultEnhancedScrollerController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using EnhancedUI;
+using EnhancedUI.EnhancedScroller;
+using UnityEngine;
+
+namespace EaseScrollView.EnhanceScrollView.Plugins
+{
+	public class DefaultEnhancedScrollerController : MonoBehaviour, IEnhancedScrollerDelegate
+	{
+		/// <summary>
+		/// This is our scroller we will be a delegate for
+		/// </summary>
+		public EnhancedScroller enhancedScroller;
+
+		/// <summary>
+		/// This will be the prefab of each cell in our scroller. Note that you can use more
+		/// than one kind of cell, but this example just has the one type.
+		/// </summary>
+		public GameObject cellViewPrefab;
+
+		protected EnhancedScrollerCellView CellViewPrefabComp;
+
+		private bool _isAwaked = false;
+		protected bool IsDirty = false;
+
+		void Awake()
+		{
+			if (cellViewPrefab == null)
+			{
+				if (enhancedScroller.Container.childCount > 0)
+				{
+					cellViewPrefab = enhancedScroller.Container.GetChild(0).gameObject;
+				}
+			}
+
+			Debug.Assert(cellViewPrefab != null, "cellViewPrefab != null");
+			var cellViewComp = cellViewPrefab.GetComponent<EnhancedScrollerCellView>();
+			if (cellViewComp == null)
+			{
+				cellViewComp = cellViewPrefab.AddComponent<EnhancedScrollerCellView>();
+			}
+
+			CellViewPrefabComp = cellViewComp;
+
+			if (enhancedScroller == null)
+			{
+				enhancedScroller = this.gameObject.GetComponent<EnhancedScroller>();
+				if (enhancedScroller == null)
+				{
+					enhancedScroller = this.gameObject.AddComponent<EnhancedScroller>();
+				}
+			}
+
+			Debug.Assert(enhancedScroller != null, "enhancedScroller != null");
+			// tell the scroller that this script will be its delegate
+			enhancedScroller.Delegate = this;
+
+			_isAwaked = true;
+		}
+
+		private void Start()
+		{
+			if (IsDirty)
+			{
+				enhancedScroller.ReloadData();
+			}
+		}
+
+		protected void InitContainer()
+		{
+		}
+
+		protected List<object> OldList;
+
+		protected void OnDataChanged(System.Collections.IList dataSources)
+		{
+			if (this.OldList == null)
+			{
+				this.OldList = new List<object>(dataSources.Count);
+			}
+			else
+			{
+				this.OldList.Clear();
+			}
+
+			foreach (var dataSource in dataSources)
+			{
+				OldList.Add(dataSource);
+			}
+
+			if (!_isAwaked)
+			{
+				IsDirty = true;
+			}
+			else
+			{
+				enhancedScroller.ReloadData();
+			}
+		}
+
+		#region EnhancedScroller Handlers
+
+		/// <summary>
+		/// This tells the scroller the number of cells that should have room allocated. This should be the length of your data array.
+		/// </summary>
+		/// <param name="scroller">The scroller that is requesting the data size</param>
+		/// <returns>The number of cells</returns>
+		public int GetNumberOfCells(EnhancedScroller scroller)
+		{
+			// in this example, we just pass the number of our data elements
+			return OldList?.Count ?? 0;
+		}
+
+		/// <summary>
+		/// This tells the scroller what the size of a given cell will be. Cells can be any size and do not have
+		/// to be uniform. For vertical scrollers the cell size will be the height. For horizontal scrollers the
+		/// cell size will be the width.
+		/// </summary>
+		/// <param name="scroller">The scroller requesting the cell size</param>
+		/// <param name="dataIndex">The index of the data that the scroller is requesting</param>
+		/// <returns>The size of the cell</returns>
+		public float GetCellViewSize(EnhancedScroller scroller, int dataIndex)
+		{
+			var cellView = this.enhancedScroller.GetCellViewAtDataIndex(dataIndex);
+			if (cellView == null)
+			{
+				cellView = this.CellViewPrefabComp;
+			}
+
+			switch (scroller.scrollDirection)
+			{
+				// in this example, even numbered cells are 30 pixels tall, odd numbered cells are 100 pixels tall
+				// return (dataIndex % 2 == 0 ? 30f : 100f);
+				case EnhancedScroller.ScrollDirectionEnum.Horizontal:
+					return ((RectTransform)cellView.transform).sizeDelta.x;
+				case EnhancedScroller.ScrollDirectionEnum.Vertical:
+					return ((RectTransform)cellView.transform).sizeDelta.y;
+				default:
+				{
+					throw new NotImplementedException();
+				}
+			}
+		}
+
+		/// <summary>
+		/// Gets the cell to be displayed. You can have numerous cell types, allowing variety in your list.
+		/// Some examples of this would be headers, footers, and other grouping cells.
+		/// </summary>
+		/// <param name="scroller">The scroller requesting the cell</param>
+		/// <param name="dataIndex">The index of the data that the scroller is requesting</param>
+		/// <param name="cellIndex">The index of the list. This will likely be different from the dataIndex if the scroller is looping</param>
+		/// <returns>The cell for the scroller to use</returns>
+		public EnhancedScrollerCellView GetCellView(EnhancedScroller scroller, int dataIndex, int cellIndex)
+		{
+			// first, we get a cell from the scroller by passing a prefab.
+			// if the scroller finds one it can recycle it will do so, otherwise
+			// it will create a new cell.
+			var cellView = scroller.GetCellView(CellViewPrefabComp);
+
+			// set the name of the game object to the cell's data index.
+			// this is optional, but it helps up debug the objects in 
+			// the scene hierarchy.
+			cellView.name = "Cell " + dataIndex.ToString();
+
+			// return the cell to the scroller
+			return cellView;
+		}
+
+		#endregion
+	}
+}
