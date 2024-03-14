0e0ef4b8cb01a81779bcc2e0d7671085b3651cf6
windy <kugoo11v12@126.com>
2024-03-14 09:14:33 +0800

    U: 支持TypeEvent

--- client/Assets/Framework/GameLib/MonoUtils/TypeEvent.cs
+++ client/Assets/Framework/GameLib/MonoUtils/TypeEvent.cs
@@ -1,24 +1,21 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace Game.Event
 {
-	/// <summary>
-	/// 事件发送器
-	/// </summary>
-	public class SEvent<TKey, TValue>
+	public class TypeEvent
 	{
 		/// <summary> 事件表 </summary>
-		private readonly Dictionary<TKey, Action<TValue>> _eventDict = new Dictionary<TKey, Action<TValue>>();
+		private readonly Dictionary<Type, object> _eventDict = new();
 
 		/// <summary> 添加事件监听器 </summary>
-		/// <param name="eventType">事件类型</param>
 		/// <param name="eventHandler">事件处理器</param>
-		public void On(TKey eventType, Action<TValue> eventHandler)
+		public void On<T>(Action<T> eventHandler)
 		{
+			var eventType = typeof(T);
 			if (_eventDict.TryGetValue(eventType, out var callbacks))
 			{
-				_eventDict[eventType] = callbacks + eventHandler;
+				_eventDict[eventType] = (Action<T>)callbacks + eventHandler;
 			}
 			else
 			{
@@ -27,13 +24,13 @@ namespace Game.Event
 		}
 
 		/// <summary> 移除事件监听器 </summary>
-		/// <param name="eventType">事件类型</param>
 		/// <param name="eventHandler">事件处理器</param>
-		public void Off(TKey eventType, Action<TValue> eventHandler)
+		public void Off<T>(Action<T> eventHandler)
 		{
+			var eventType = typeof(T);
 			if (_eventDict.TryGetValue(eventType, out var callbacks))
 			{
-				callbacks = (Action<TValue>)Delegate.RemoveAll(callbacks, eventHandler);
+				callbacks = (Action<T>)Delegate.RemoveAll((Action<T>)callbacks, eventHandler);
 				if (callbacks == null)
 				{
 					_eventDict.Remove(eventType);
@@ -46,35 +43,42 @@ namespace Game.Event
 		}
 
 		/// <summary> 是否已经拥有该类型的事件监听器 </summary>
-		/// <param name="eventType">事件名称</param>
-		public bool IsExist(TKey eventType)
+		public bool IsExist<T>()
 		{
+			var eventType = typeof(T);
 			return _eventDict.ContainsKey(eventType);
 		}
 
 		/// <summary> 发送事件 </summary>
-		/// <param name="eventType">事件类型</param>
 		/// <param name="eventArg">事件参数</param>
-		public void Send(TKey eventType, TValue eventArg)
+		public void Send<T>(T eventArg)
 		{
+			var eventType = typeof(T);
 			if (_eventDict.TryGetValue(eventType, out var callbacks))
 			{
-				callbacks.Invoke(eventArg);
+				((Action<T>)callbacks).Invoke(eventArg);
 			}
 		}
 
-		public void DriveHead(TKey eventType, TValue eventArg)
+		/// <summary>
+		/// 只向第一个监听发送事件
+		/// </summary>
+		/// <param name="eventArg"></param>
+		/// <typeparam name="T"></typeparam>
+		public void DriveHead<T>(T eventArg)
 		{
+			var eventType = typeof(T);
 			if (_eventDict.TryGetValue(eventType, out var callbacks))
 			{
 				if (callbacks != null)
 				{
-					var list = callbacks.GetInvocationList();
+					var action = (Action<T>)callbacks;
+					var list = action.GetInvocationList();
 					if (list.Length > 0)
 					{
 						var call0 = list[0];
-						var call = call0 as Action<TValue>;
-						_eventDict[eventType] = callbacks - call;
+						var call = call0 as Action<T>;
+						_eventDict[eventType] = action - call;
 						call(eventArg);
 					}
 				}
